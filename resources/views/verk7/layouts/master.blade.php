420a8d4006e486c52611cc56823a764d29b7173a
Eggert Geir Axelsson <eggertgeir@gmail.com>
2017-11-08 11:01:42 +0000

    Add files via upload

--- resources/views/verk7/layouts/master.blade.php
+++ resources/views/verk7/layouts/master.blade.php
@@ -5,71 +5,12 @@
     <title>@yield('title')</title>
       <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-            ul{
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 20px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-                text-decoration: none;
-            }
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            img{
-                width: 50%;
-            }
+            
         </style>
+      <link href="/css/style.css" rel="stylesheet">
   </head>
   <body>
+      @include ('verk7.layouts.nav')
     <ul>
       @yield('list')
     </ul>
