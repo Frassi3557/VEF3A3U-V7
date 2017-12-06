97aca797933f875efde5ca34b42aafdc9d033c03
Eggert Axelsson <Eggert Axelsson>
2017-12-06 00:10:46 +0000

    search bar bara li

--- resources/views/verk7/layouts/clickbait.blade.php
+++ resources/views/verk7/layouts/clickbait.blade.php
@@ -1,14 +1,12 @@
 <div class="news-box">
-    <a href="/verk7/posts/1"><img src="<?= $persons[0][1] ?>"></a>
-        <li><a href="/verk7/posts/1"><?= $persons[0][0]; ?></a></li>
+	<a href="/verk7/posts/1"><img src="<?= $persons[0][1] ?>"></a>
+	<li><a href="/verk7/posts/1"><?= $persons[0][0]; ?></a></li>
 </div>
 <div class="news-box">
-    <a href="/verk7/posts/2"><img src="<?= $persons[1][1] ?>"></a>
-        <li><a href="/verk7/posts/2"><?= $persons[1][0]; ?></a></li>
+	<a href="/verk7/posts/2"><img src="<?= $persons[1][1] ?>"></a>
+	<li><a href="/verk7/posts/2"><?= $persons[1][0]; ?></a></li>
 </div>
 <div class="news-box">
-    
-        <a href="/verk7/posts/3"><img src="<?= $persons[2][1] ?>"></a>
-        <li><a href="/verk7/posts/3"><?= $persons[2][0]; ?></a></li>
-    
+	<a href="/verk7/posts/3"><img src="<?= $persons[2][1] ?>"></a>
+	<li><a href="/verk7/posts/3"><?= $persons[2][0]; ?></a></li>
 </div>
